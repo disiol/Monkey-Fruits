8d6600d1c6d61e90f0f43fcb83fb04b414688b04
denis <025896321Denis>
2019-12-19 13:22:03 +0200

    app push

--- app/src/main/java/com/monkey/fruits/constants/Constants.java
+++ app/src/main/java/com/monkey/fruits/constants/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
     public static final String MYLOG_TEG = "my log";
     public static final String URL_FOR_CHECK = "https://gpposhadi5.xyz/DpXsmBr3/";
     public static final String URI_STRING = "https://traffidomn.xyz/kY3H29Xg";
-
+    public static final String DEPLINK = "pls://run";
 
 }
--- app/src/main/java/com/monkey/fruits/ui/activities/MainActivity.java
+++ app/src/main/java/com/monkey/fruits/ui/activities/MainActivity.java
@@ -1,8 +1,13 @@
 package com.monkey.fruits.ui.activities;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.facebook.applinks.AppLinkData;
+import com.monkey.fruits.BuildConfig;
 import com.monkey.fruits.manedger.PreferencesManager;
 import com.monkey.fruits.routers.main.MainActivityRouter;
 import com.monkey.fruits.ui.base.BaseActivity;
@@ -10,6 +15,9 @@ import com.monkey.fruits.R;
 
 
 import javax.inject.Inject;
+
+import static com.monkey.fruits.constants.Constants.DEPLINK;
+import static com.monkey.fruits.constants.Constants.URI_STRING;
 
 
 public class MainActivity extends BaseActivity {
@@ -23,7 +31,89 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // ATTENTION: This was auto-generated to handle app links.
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+
+        try {
+            Uri appLinkData2 = appLinkIntent.getData();
+            if (appLinkData2 != null) {
+                String url = appLinkData2.toString();
+                if (BuildConfig.DEBUG) {
+                    Log.d("my Log" + this.getLocalClassName(), "App Link appLinkData: " + url);
+                }
+                String string = convertArrayToStringMethod(url.split(DEPLINK));
+
+                if (BuildConfig.DEBUG) {
+                    Log.d("MyLog" + this.getLocalClassName(), "App Link appLinkData url: " + url);
+
+                    Log.d("MyLog" + this.getLocalClassName(), "App Link appLinkData string: " + string);
+                }
+                String token = URI_STRING + string;
+
+                if (BuildConfig.DEBUG) {
+                    Log.d("MyLog" + this.getLocalClassName(), "App Link appLinkData token: " + token);
+                }
+
+                preferencesManager.setURL(token);
+
+
+            }
+
+            AppLinkData.fetchDeferredAppLinkData(this, appLinkData -> {
+                AppLinkData appLinkData1 = appLinkData;
+                if (appLinkData1 == null || appLinkData1.getTargetUri() == null) {
+                    Log.e("MyLog", "deeplink = null");
+
+
+                } else {
+
+                    String url = appLinkData1.getTargetUri().toString();
+                    if (BuildConfig.DEBUG) {
+                        Log.d("MyLog", "deeplink = " + url);
+
+                        Log.d("my Log" + this.getLocalClassName(), "App Link appLinkData: " + url);
+                    }
+                    String string = convertArrayToStringMethod(url.split(DEPLINK));
+
+                    if (BuildConfig.DEBUG) {
+                        Log.d("MyLog" + this.getLocalClassName(), "App Link appLinkData url: " + url);
+
+                        Log.d("MyLog" + this.getLocalClassName(), "App Link appLinkData string: " + string);
+                    }
+                    String token = URI_STRING + string;
+
+                    if (BuildConfig.DEBUG) {
+                        Log.d("MyLog" + this.getLocalClassName(), "App Link appLinkData token: " + token);
+                    }
+
+                    preferencesManager.setURL(token);
+                }
+            });
+        } catch (Exception e) {
+            Log.e("my Log" + this.getLocalClassName(), "App Link appLinkData: " + e.toString());
+
+            e.printStackTrace();
+        }
+
+
         mainActivityRouter.showLogoFragment();
+
+    }
+
+    public static String convertArrayToStringMethod(String[] strArray) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < strArray.length; i++) {
+
+            stringBuilder.append(strArray[i]);
+
+        }
+
+        return stringBuilder.toString();
 
     }
 
